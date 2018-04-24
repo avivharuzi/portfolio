6bad7ebacaa3f5bec3d97c49ee63158a8c7e0ebf
avivharuzi <avivharuzi@gmail.com>
2018-04-24 12:17:41 +0300

    contact form added

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,2 +1,20 @@
 <template>
+  <div class="content">
+    <div class="content-center row justify-content-center">
+      <div class="col-md-6 absolute-center">
+        <form>
+          <div class="form-group">
+            <input type="text" class="form-control form-control-lg" placeholder="Full Name" autofocus>
+          </div>
+          <div class="form-group">
+            <input type="email" class="form-control form-control-lg" placeholder="Email">
+          </div>
+          <div class="form-group">
+            <textarea class="form-control form-control-lg" placeholder="Message"></textarea>
+          </div>
+          <button class="btn btn-lg btn-success">Send</button>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
