9a83486013d77a7166fc80a945979f2bd4d66bac
avivharuzi <avivharuzi@gmail.com>
2018-04-26 10:01:49 +0300

    title changed and facebook link changed

--- client/src/components/SocialLinks.vue
+++ client/src/components/SocialLinks.vue
@@ -14,7 +14,7 @@
       return {
         socialLinks: [
           { name: 'twitter', link: 'https://twitter.com/aviv_haruzi' },
-          { name: 'facebook', link: 'https://www.facebook.com/aviv.haruzi' },
+          { name: 'facebook', link: 'https://www.facebook.com/people/Aviv-Haruzi/100001590224128' },
           { name: 'linkedin', link: 'https://www.linkedin.com/in/aviv-haruzi-33589915b' },
           { name: 'instagram', link: 'https://www.instagram.com/aviv_haruzi' },
           { name: 'github', link: 'https://github.com/avivharuzi' }
--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -3,7 +3,7 @@
     <app-social-links></app-social-links>
     <div id="mainHeader">
       <h1>AVIV HARUZI</h1>
-      <h2>FULL STACK DEVELOPER</h2>
+      <h2>FULL STACK WEB DEVELOPER</h2>
     </div>
   </div>
 </template>
